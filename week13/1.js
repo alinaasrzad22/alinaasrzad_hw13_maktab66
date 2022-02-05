b7c74bb7f9cf037f518369748434ab35f5f71070
alinaasrzad22 <96845617+alinaasrzad22@users.noreply.github.com>
2022-02-05 13:12:05 +0330

    Add files via upload

---
+++ week13/1.js
@@ -0,0 +1,5 @@
+const deletTr=document.getElementById("trhtm");
+const btn=document.createElement("button");
+btn.classList.add("btn", "btn-danger");
+const textBtn=document.createTextNode("Delete");
+btn.appendChild(textBtn);
